7527ce452c5dba95ba75cd3ec531334c6f081fd7
matthew-maya-17 <matthewmatt2009@hotmail.com>
2025-11-07 16:16:14 -0500

    feat: set up user api: model, repository, service, controller layers

--- Brewview/src/main/java/com/service/UserService.java
+++ Brewview/src/main/java/com/service/UserService.java
@@ -1,4 +1,22 @@
 package com.service;
 
+import com.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    //CREATE
+
+    //READ
+
+    //UPDATE
+
+    //DELETE
 }
