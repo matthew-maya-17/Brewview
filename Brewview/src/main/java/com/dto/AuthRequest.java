c7c3642b563cc136342ab5f6dd7d8ec146396f0c
matthew-maya-17 <matthewmatt2009@hotmail.com>
2025-11-19 17:03:52 -0500

    feat: created jwt token and user authentication classes

---
+++ Brewview/src/main/java/com/dto/AuthRequest.java
@@ -0,0 +1,33 @@
+package com.dto;
+
+import lombok.Data;
+
+public class AuthRequest {
+    private String username;
+    private String password;
+
+    public AuthRequest() {}
+
+    public AuthRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getter and Setter for username
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // Getter and Setter for password
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- Brewview/src/main/java/com/repository/UserRepository.java
+++ Brewview/src/main/java/com/repository/UserRepository.java
@@ -4,7 +4,12 @@ import com.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, String> {
+import java.util.Optional;
+import java.util.UUID;
 
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findUserByUsername(String username);
+    Optional<User> findUserByEmail(String email);
 }
