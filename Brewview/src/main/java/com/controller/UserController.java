0519674355003712930a4586b288e34771d6245f
matthew-maya-17 <matthewmatt2009@hotmail.com>
2025-11-24 20:33:24 -0500

    feat: updated JWT auth to fit architecture

--- Brewview/src/main/java/com/controller/UserController.java
+++ Brewview/src/main/java/com/controller/UserController.java
@@ -1,51 +1,25 @@
 package com.controller;
 
-import com.dto.AuthRequest;
-import com.model.User;
-import com.security.JwtService;
-import com.security.UserInfoService;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import com.dto.CreateUserRequest;
+import com.dto.ResponseUser;
+import com.service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/api/user")
+@RequestMapping(path = "/api/users")
 public class UserController {
 
-    private final UserInfoService userService;
+    private final UserService userService;
 
-    private final JwtService jwtService;
-
-    private final AuthenticationManager authenticationManager;
-
-    public UserController(UserInfoService userService,
-                          JwtService jwtService,
-                          AuthenticationManager authenticationManager) {
+    public UserController(UserService userService) {
         this.userService = userService;
-        this.jwtService = jwtService;
-        this.authenticationManager = authenticationManager;
     }
 
-    @PostMapping("/addNewUser")
-    public String addNewUser(@RequestBody User userInfo) {
+    @PostMapping("/addUser")
+    public ResponseUser addNewUser(@RequestBody CreateUserRequest userInfo) {
         return userService.addUser(userInfo);
-    }
-
-    // Removed the role checks here as they are already managed in SecurityConfig
-    @PostMapping("/generateToken")
-    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
-        );
-        if (authentication.isAuthenticated()) {
-            return jwtService.generateToken(authRequest.getUsername());
-        } else {
-            throw new UsernameNotFoundException("Invalid user request!");
-        }
     }
 }
